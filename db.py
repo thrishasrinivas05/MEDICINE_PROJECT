427b085ac3d5d35ad5f247e5931398ce594ad7eb
thrishasrinivas05 <thrishasrinivas05@gmail.com>
2026-05-05 23:30:03 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
+
+# 🔥 USE ENV VARIABLE (RENDER SAFE)
+MONGO_URI = os.environ.get("MONGO_URI")
+
+client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+db = client["medicineDB"]
+
+medicine_collection = db["medicines"]
+user_collection = db["users"]
+
+
+# ================= MEDICINES =================
+def insert_medicine(data):
+    medicine_collection.insert_one(data)
+
+
+def get_all_medicines():
+    return list(medicine_collection.find())
+
+
+def delete_medicine(id):
+    medicine_collection.delete_one({"_id": ObjectId(id)})
+
+
+# ================= USERS =================
+def insert_user(data):
+    user_collection.insert_one(data)
+
+
+def get_user(email):
+    return user_collection.find_one({"email": email})
